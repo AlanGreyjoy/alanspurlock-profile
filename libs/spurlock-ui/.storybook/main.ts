542f9036eb16c70a9e03e0a44de48f0e1190ec70
Alan Spurlock <alanspurlock@hotmail.com>
2025-12-06 14:22:58 -0600

    feat: Refactor Storybook configuration to simplify options and improve module path handling.

--- libs/spurlock-ui/.storybook/main.ts
+++ libs/spurlock-ui/.storybook/main.ts
@@ -1,19 +1,20 @@
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
+import { createRequire } from 'node:module';
 
 import type { StorybookConfig } from '@storybook/react-vite';
 import type { InlineConfig } from 'vite';
+
+const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
   addons: [],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
-    options: {
-      builder: {
-        viteConfigPath: 'libs/spurlock-ui/vite.config.mts',
-      },
-    },
+    options: {},
   },
   async viteFinal(config: InlineConfig) {
     return {
