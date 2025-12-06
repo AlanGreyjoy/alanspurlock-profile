6761294b711df36233fa3e6eba6691fcb3355533
Alan Spurlock <alanspurlock@hotmail.com>
2025-12-06 17:35:46 -0600

    refactor: Simplify Storybook configuration by removing unnecessary imports and using direct module imports for Tailwind CSS and autoprefixer

--- libs/spurlock-ui/.storybook/main.ts
+++ libs/spurlock-ui/.storybook/main.ts
@@ -1,22 +1,14 @@
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-import { createRequire } from 'node:module';
-
+import { join } from 'path';
 import type { StorybookConfig } from '@storybook/react-vite';
-
-const require = createRequire(import.meta.url);
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const config: StorybookConfig = {
   stories: [
     '../src/Introduction.stories.tsx',
     '../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
   ],
-  addons: [getAbsolutePath('@storybook/addon-essentials')],
+  addons: ['@storybook/addon-essentials'],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: '@storybook/react-vite',
     options: {},
   },
   core: {
@@ -25,23 +17,22 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
-  async viteFinal(config) {
+  async viteFinal(config, { configType }) {
+    const { default: tailwindcss } = await import('tailwindcss');
+    const { default: autoprefixer } = await import('autoprefixer');
+
     return {
       ...config,
       css: {
         postcss: {
           plugins: [
-            require('tailwindcss')(join(__dirname, '../tailwind.config.js')),
-            require('autoprefixer'),
+            tailwindcss(join(__dirname, '../tailwind.config.js')),
+            autoprefixer(),
           ],
         },
       },
     };
   },
 };
-
-function getAbsolutePath(value: string): any {
-  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
-}
 
 export default config;
